09ee4bc72d9b89257cf764c4d9802b377c0e3b95
Sameep1234 <sameep0112vani@gmail.com>
2021-05-27 17:32:24 +0530

    Started home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import { Card } from 'reactstrap';
 
 class Home extends Component {
     render() {
-        return(
-            <div>
-                Landed Here!!
+        return (
+            <div className="container">
+                <h2 className="d-flex justify-content-center">For the love of fit</h2>
+                <p className="mx-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit. Suspendisse id ex elit. Suspendisse laoreet
+                    venenatis malesuada. Ut venenatis in nisi a semper.
+                    Nam vitae sodales mauris, fringilla ullamcorper
+                    metus. Sed vel vulputate metus, molestie volutpat
+                    mauris. Integer luctus aliquam ornare. Maecenas
+                    malesuada nunc mollis, pellentesque neque in,
+                    eleifend nunc. Proin fringilla laoreet iaculis.
+                    Mauris posuere ex pharetra, scelerisque ligula
+                    vitae, sodales arcu. Nam scelerisque lorem augue,
+                    id dapibus massa ultricies porttitor. Nam eu rutrum
+                    eros. Suspendisse et lectus ut sapien aliquet
+                    placerat. Pellentesque rhoncus quis orci quis
+                    posuere. Quisque at semper nisi. Cras auctor,
+                    lorem non molestie faucibus, purus eros consequat
+                    mi, eget maximus eros orci vitae ipsum.
+                </p>
+                <div className="mt-3" id="container">
+                    <Card style={{border: 'white'}}>
+                        <div className="row">
+                            <h3 className="col-10" style={{ fontFamily: 'cursive' }}>Subscriptions</h3>
+                            <a href="/" className="col-2 mt-1 text-decoration-none d-flex justify-content-end" role="button" style={{ fontFamily: 'cursive' }}>Explore</a>
+                        </div>
+                    </Card>
+                </div>
             </div>
         );
     }
--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -6,11 +6,13 @@ import Home from './Home';
 class Main extends Component {
     render() {
         return (
-            <Switch location={window.location} key={window.location.pathname}>
+            <div>
                 <Header />
-                <Route path="/home" component={Home} />
-                <Redirect to="/home" />
-            </Switch>
+                <Switch location={window.location} key={window.location.pathname}>
+                    <Route exact path="/home" component={Home} />
+                    <Redirect to="/home" />
+                </Switch>
+            </div>
         );
     }
 }
