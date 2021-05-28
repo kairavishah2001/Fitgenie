5673934fd8ec1988d61f2ef16693cf23b7e8823a
Sameep1234 <sameep0112vani@gmail.com>
2021-05-28 08:59:05 +0530

    Completed Home Page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -66,6 +66,61 @@ class Home extends Component {
                         </Card>
                     </div>
                 </div>
+                <div className='d-flex justify-content-center' data-aos="fade-up">
+                    <div className="mt-3 col-md-6">
+                        <Card style={{ border: 'white' }}>
+                            <div className="row">
+                                <h3 className="col-10" style={{ fontFamily: 'cursive' }}>Eat</h3>
+                                <a href="/" className="stretched-link col-2 mt-1 text-decoration-none d-flex justify-content-end" role="button" style={{ fontFamily: 'cursive' }}>Explore</a>
+                            </div>
+                            <h6>Healthy, Tasty, Everyday Food!</h6>
+                            <p style={{fontSize: '10px'}}>Indulge in healthy meals that are both nutritious and delicious</p>
+                            <div className="d-flex justify-content-center mb-2">
+                                
+                                <CardImg style={{borderRadius: '2%'}} src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_600,h_470,fl_progressive,f_auto,q_auto:eco/dpr_2/image/vm/257e0d59-ebf5-406d-a775-ba22ed951dc8.png" />
+                                <CardImgOverlay>
+                                    <CardText>
+                                        <div className="text-light pt-5 mt-5">
+                                            <CardText><strong>Home Style</strong></CardText>
+                                            <small style={{color: 'whitesmoke'}}>Nothing can beat the Great Indian Thali. Enjoy <br />
+                                                homestyle food just like mum's, made with less <br />
+                                                oil and mild spices. <br />
+                                            </small>
+                                        </div>
+                                    </CardText>
+                                </CardImgOverlay>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+                <div className='d-flex justify-content-center' data-aos="fade-up">
+                    <div className="mt-3 col-md-6">
+                        <Card style={{ border: 'white' }}>
+                            <div className="row">
+                                <h3 className="col-10" style={{ fontFamily: 'cursive' }}>Care</h3>
+                                <a href="/" className="stretched-link col-2 mt-1 text-decoration-none d-flex justify-content-end" role="button" style={{ fontFamily: 'cursive' }}>Explore</a>
+                            </div>
+                            <h6>Making health rasy and accessible</h6>
+                            <p style={{fontSize: '10px'}}>Book online doctor consultations and schedule diagnostic tests at home.</p>
+                            <div className="d-flex justify-content-center mb-2">
+                                
+                                <CardImg style={{borderRadius: '2%'}} src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_600,h_470,fl_progressive,f_auto,q_auto:eco/dpr_2/image/vm/0f4c194a-3d2c-453c-8a3a-fdf20bd03699.png" />
+                                <CardImgOverlay>
+                                    <CardText>
+                                        <div className="pt-5 mt-5">
+                                            <CardText><strong>Online Doctor Consultation</strong></CardText>
+                                            <small>Consilt with our doctors and specialists <br />
+                                                orthopedics, pediatricians and gynaecologists <br />
+                                                for all your medical needs with minimum wait <br />
+                                                time.
+                                            </small>
+                                        </div>
+                                    </CardText>
+                                </CardImgOverlay>
+                            </div>
+                        </Card>
+                    </div>
+                </div>
             </div>
         );
     }
